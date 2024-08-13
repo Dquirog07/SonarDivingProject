9abbdf36d170644057e4952cb2965958b854bb0b
Diana <75185479+Dquirog07@users.noreply.github.com>
2024-08-13 18:35:21 -0400

    Update Sonar_iot_controller.py
    Thinking about making some changes, there may be deadlock issues

--- Sonar_iot_controller.py
+++ Sonar_iot_controller.py
@@ -5,6 +5,10 @@ from mysql.connector import Error
 import random
 from datetime import datetime
 import time
+from cassandra_setup import setup_cassandra
+import threading
+import uuid
+import pdb
 
 def load_config(file_path='config.json'):
     """ Load configuration from a JSON file. """
@@ -12,13 +16,18 @@ def load_config(file_path='config.json'):
         return json.load(file)
 
 config = load_config()
+kafka_conf = config['kafka']
+producer = Producer(**{'bootstrap.servers': kafka_conf['bootstrap.servers']})
+print('producer initialized')
+
 
 def acked(err, msg):
     """ Callback function for Kafka produce acknowledgement. """
     if err is not None:
         print("Failed to deliver message: %s: %s" % (str(msg), str(err)))
     else:
-        print("Message produced: %s" % (str(msg)))
+        None
+        # print("Message produced: %s" % (str(msg)))
 
 def buildConnection():
     """ Create and return a MySQL database connection using the loaded configuration. """
@@ -31,29 +40,66 @@ def buildConnection():
     )
     return connection
 
-def kafka_producer():
-    """ Kafka Producer to simulate sonar data using the loaded configuration. """
-    kafka_conf = config['kafka']
-    producer = Producer(**kafka_conf)
-    topic = kafka_conf['topic']
+def get_diver_info():
     try:
-        depth = random.uniform(5, 100)  # Depth in meters
-        sound_signature = random.randint(1000, 9999)  # Simulate a sonar signature
-        data = {
-            'timestamp': str(datetime.now()),
-            'depth': depth,
-            'sound_signature': sound_signature
-        }
-        producer.produce(topic, json.dumps(data).encode('utf-8'))
+        connection = buildConnection()
+        cursor = connection.cursor()
+
+        diver_sql = "SELECT diver_id FROM divers LIMIT 1"
+        cursor.execute(diver_sql)
+        diver_id = cursor.fetchone()
+
+        dive_session_sql = "SELECT dive_id FROM dives WHERE diver_id = %s AND status = 'Active' LIMIT 1"
+        cursor.execute(dive_session_sql, (diver_id[0],))
+        dive_id = cursor.fetchone()
+
+        return diver_id[0], dive_id[0]
+
+    except mysql.connector.Error as err:
+        print("MySQL Error: ", err)
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+
+
+def produce_sonar_data(data):
+    try:
+        topic = kafka_conf['producer']
+        topic = topic['topics']
+
+        event_json = json.dumps(data)
+        producer.produce('sonar_data', event_json.encode('utf-8'), callback=acked)
         producer.flush()
-        print(f"Produced: {data}")
     except Exception as e:
         print("Failed to produce: ", e)
 
+def collect_sonar_data(diver_id, dive_id):
+    """ Simulate collecting sonar data. """
+    depth = random.uniform(5, 100)  # Depth in meters
+    sound_signature = random.randint(1000, 1010)  # Simulate a sonar signature
+    data = {
+        'timestamp': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+        'diver_id': diver_id,
+        'dive_id': dive_id,
+        'sonar_data': { 
+            'depth': depth,
+            'sound_signature': sound_signature}
+    }
+    return data
+
 def main():
+
+    diver_id, dive_id = get_diver_info()
+
     while True:
-        kafka_producer()
-        time.sleep(config['app_settings']['poll_interval_seconds'])  # Pause based on config setting
+        if diver_id and dive_id:
+            sonar_data = collect_sonar_data(diver_id, dive_id)
+            produce_sonar_data(sonar_data)
+
+        time.sleep(config['app_settings']['poll_interval_seconds'])
+
+        
 
 if __name__ == '__main__':
     main()

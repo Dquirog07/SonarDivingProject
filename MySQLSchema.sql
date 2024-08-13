a766a4813ac5db399c95a99f78b9d79a50c63ef9
Diana <75185479+Dquirog07@users.noreply.github.com>
2024-08-13 18:45:55 -0400

    Update MySQLSchema.sql
    Simplified to get it working in pieces, still need to update the status and timestamp after end dive

--- MySQLSchema.sql
+++ MySQLSchema.sql
@@ -2,8 +2,7 @@
 CREATE TABLE IF NOT EXISTS divers (
     diver_id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    certification_number VARCHAR(100),
-    experience_level VARCHAR(100)
+    profile_data JSON
 );
 
 
@@ -12,39 +11,7 @@ CREATE TABLE IF NOT EXISTS dives (
     diver_id INT NOT NULL,
     start_timestamp DATETIME,
     end_timestamp DATETIME,
-    start_location VARCHAR(255),
-    end_location VARCHAR(255),
+    status ENUM('Active', 'Inactive', 'Completed')
     FOREIGN KEY (diver_id) REFERENCES divers(diver_id)
-);
-
-
-CREATE TABLE IF NOT EXISTS events (
-    event_id INT AUTO_INCREMENT PRIMARY KEY,
-    dive_id INT NOT NULL,
-    event_timestamp DATETIME,
-    event_type ENUM('start', 'cookie_placement', 'exit_detection'),
-    location VARCHAR(255),
-    details TEXT,
-    FOREIGN KEY (dive_id) REFERENCES dives(dive_id)
-);
-
-
-CREATE TABLE IF NOT EXISTS cookies (
-    cookie_id INT AUTO_INCREMENT PRIMARY KEY,
-    dive_id INT NOT NULL,
-    placement_timestamp DATETIME,
-    location VARCHAR(255),
-    material TEXT,
-    FOREIGN KEY (dive_id) REFERENCES dives(dive_id)
-);
-
-CREATE TABLE IF NOT EXISTS sonar_readings (
-    reading_id INT AUTO_INCREMENT PRIMARY KEY,
-    dive_id INT NOT NULL,
-    timestamp DATETIME NOT NULL,
-    depth DECIMAL(5, 2) NOT NULL,  
-    sound_profile BLOB,            
-    FOREIGN KEY (dive_id) REFERENCES dives(dive_id),
-    INDEX (timestamp)             
 );
 
